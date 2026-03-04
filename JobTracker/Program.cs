8a7cf5d72e1f1f5617dfc90652821d3acb854127
fajar_kkp <fajarputra4231@gmail.com>
2026-03-04 21:12:00 +0700

    Initial MVP at Program.cs

---
+++ JobTracker/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Collections.Generic;
+
+enum JobStatus
+{
+    Applied,
+    Interview,
+    Rejected,
+    Offer
+}
+
+class Job
+{
+    public int Id {get; set;}
+    public string CompanyName {get; set;}
+
+    public string Position {get; set;}
+    public JobStatus Status {get; set;}
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
