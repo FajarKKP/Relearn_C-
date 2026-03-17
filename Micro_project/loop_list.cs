89716d6305a5a6e75205d63b80bbcb526b944fc5
fajar_kkp <fajarputra4231@gmail.com>
2026-03-17 13:20:56 +0700

    Day 1 done

---
+++ Micro_project/loop_list.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> dummyList = new List<int> {1,2,4,7,3,5};
+
+// Console.WriteLine(string.Join(",",dummyList));
+
+// foreach (var i in dummyList)
+// {
+//     Console.WriteLine(i);
+// }
+
+Console.WriteLine(dummyList.Max());
