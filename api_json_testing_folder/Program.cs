78db2cde0a13ef456d898a4b36b4924ce26418e1
fajar_kkp <fajarputra4231@gmail.com>
2026-04-03 15:06:53 +0700

    learn to deserialize from json.

--- api_json_testing_folder/Program.cs
+++ api_json_testing_folder/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+var json = "{\"name\":\"John\",\"age\":\"30\"}";
+
+// Console.WriteLine(json);
+
+Person? person = JsonSerializer.Deserialize<Person>(json);
+
+Console.WriteLine(person?.age);
+
+public class Person
+{
+    public string? name {get; set;}
+    public string? age {get; set;}
+}
